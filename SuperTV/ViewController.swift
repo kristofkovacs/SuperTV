d2c6220d47bf34176f9e32855d8b4521ea11a34f
kristofkovacs <kovacsegrikristof@gmail.com>
2017-07-18 18:14:01 +0200

    initial commit

---
+++ SuperTV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SuperTV
+//
+//  Created by Kovacs-Egri, Kristof on 2017. 07. 18..
+//  Copyright © 2017. Kovacs-Egri, Kristof. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
